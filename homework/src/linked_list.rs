628c6d3fa0495dccbc3829aedbedc61b188bb880
SU-BIN LEE <subinlee2631@kaist.ac.kr>
2023-10-01 18:17:55 +0900

    fill next in IterMut

--- homework/src/linked_list.rs
+++ homework/src/linked_list.rs
@@ -684,7 +684,16 @@ impl<'a, T> Iterator for IterMut<'a, T> {
 
     #[inline]
     fn next(&mut self) -> Option<&'a mut T> {
-        todo!()
+        // todo!()
+        if self.len == 0 {
+            None
+        } else {
+            unsafe { self.head.as_mut() }.map(|node| {
+                self.len -= 1;
+                self.head = node.next;
+                &mut node.element
+            })
+        }
     }
 }
 

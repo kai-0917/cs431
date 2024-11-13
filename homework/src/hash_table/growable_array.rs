9d5a92cc80c354ff9ab4f6c4e03705facd4f45c9
kai-0917 <subinlee2631@kaist.ac.kr>
2024-11-13 22:23:30 +0900

    complete hash_table

--- homework/src/hash_table/growable_array.rs
+++ homework/src/hash_table/growable_array.rs
@@ -149,6 +149,36 @@ impl Segment {
             },
         }
     }
+
+    fn free_all(&mut self) {
+        for i in 0..(1 << 10) {
+            let curr = (*self)[i].load(Ordering::SeqCst);
+            if curr != 0 {
+                unsafe {
+                    (*(curr as *mut Segment)).free_all();
+                }
+            }
+        }
+        unsafe {
+            drop(Box::from_raw(self as *mut Segment));
+        }
+    }
+
+    fn free_with_level(&mut self, l: usize) {
+        if l != 0 {
+            for i in 0..(1 << 10) {
+                let curr = (*self)[i].load(Ordering::SeqCst);
+                if curr != 0 {
+                    unsafe {
+                        (*(curr as *mut Segment)).free_with_level(l - 1);
+                    }
+                }
+            }
+        }
+        unsafe {
+            drop(Box::from_raw(self as *mut Segment));
+        }
+    }
 }
 
 impl Deref for Segment {
@@ -174,7 +204,16 @@ impl Debug for Segment {
 impl<T> Drop for GrowableArray<T> {
     /// Deallocate segments, but not the individual elements.
     fn drop(&mut self) {
-        todo!()
+        // todo!()
+        let mut guard = crossbeam_epoch::pin();
+        let mut root = self.root.load(Ordering::SeqCst, &guard);
+        let height = root.tag();
+        unsafe {
+            let mut raw = root.with_tag(0).as_raw() as usize;
+            if raw != 0 {
+                (*(raw as *mut Segment)).free_with_level(height - 1);
+            }
+        }
     }
 }
 
@@ -196,6 +235,102 @@ impl<T> GrowableArray<T> {
     /// Returns the reference to the `Atomic` pointer at `index`. Allocates new segments if
     /// necessary.
     pub fn get(&self, mut index: usize, guard: &Guard) -> &Atomic<T> {
-        todo!()
+        // todo!()
+        let mut v = Vec::new();
+        if index == 0 {
+            v.insert(0, 0);
+        }
+        while index != 0 {
+            v.insert(0, index & ((1 << 10) - 1));
+            index >>= 10;
+        }
+        loop {
+            // println!("index {:?}", v);
+            let ori_root = self.root.load(Ordering::SeqCst, guard);
+            let height = ori_root.tag();
+            // increase the height
+            if height < v.len() {
+                let mut new_root = Box::into_raw(Box::new(Segment::new()));
+                let mut leaf_seg_ptr1 = new_root;
+                let mut l1: usize = 0;
+                if height != 0 {
+                    while l1 < v.len() - height - 1 {
+                        unsafe {
+                            let new_seg = Box::into_raw(Box::new(Segment::new()));
+                            (**leaf_seg_ptr1)[0].store(new_seg as usize, Ordering::SeqCst);
+                            leaf_seg_ptr1 = new_seg;
+                        }
+                        l1 += 1;
+                    }
+                    unsafe {
+                        (**leaf_seg_ptr1)[0].store(ori_root.as_raw() as usize, Ordering::SeqCst);
+                    }
+                }
+                unsafe {
+                    if self
+                        .root
+                        .compare_exchange(
+                            ori_root,
+                            Shared::from_usize(new_root as usize).with_tag(v.len()),
+                            Ordering::SeqCst,
+                            Ordering::SeqCst,
+                            guard,
+                        )
+                        .is_err()
+                    {
+                        (**leaf_seg_ptr1)[0].store(0, Ordering::SeqCst);
+                        // (*new_root).free_all();
+                        (*new_root).free_with_level(v.len() - 1);
+                        continue;
+                    }
+                }
+                continue;
+            }
+            // not need to increase the height
+            for i in 0..(height - v.len()) {
+                v.insert(0, 0);
+            }
+            let mut leaf_seg_ptr = ori_root.as_raw() as *mut Segment;
+            let mut l = 0;
+            loop {
+                if l >= v.len() - 1 {
+                    break;
+                }
+                unsafe {
+                    let ptr = (**leaf_seg_ptr)[v[l]].load(Ordering::SeqCst);
+                    if ptr == 0 {
+                        break;
+                    }
+                    leaf_seg_ptr = ptr as *mut Segment;
+                    l += 1;
+                }
+            }
+            let remaining = v.len() - l - 1;
+            if remaining != 0 {
+                let mut new_seg = Box::into_raw(Box::new(Segment::new()));
+                let mut leaf_seg_ptr1 = new_seg;
+                for i in 0..(remaining - 1) {
+                    unsafe {
+                        let new_seg = Box::into_raw(Box::new(Segment::new()));
+                        (**leaf_seg_ptr1)[v[l + i + 1]].store(new_seg as usize, Ordering::SeqCst);
+                        leaf_seg_ptr1 = new_seg;
+                    }
+                }
+                unsafe {
+                    if (**leaf_seg_ptr)[v[l]]
+                        .compare_exchange(0, new_seg as usize, Ordering::SeqCst, Ordering::SeqCst)
+                        .is_err()
+                    {
+                        // (*new_seg).free_all();
+                        (*new_seg).free_with_level(v.len() - 1);
+                        continue;
+                    }
+                }
+                leaf_seg_ptr = leaf_seg_ptr1;
+            }
+            return unsafe {
+                &*((*leaf_seg_ptr).get_unchecked(v[v.len() - 1]) as *const _ as *const Atomic<T>)
+            };
+        }
     }
 }
--- homework/src/hash_table/split_ordered_list.rs
+++ homework/src/hash_table/split_ordered_list.rs
@@ -2,8 +2,12 @@
 
 use core::mem;
 use core::sync::atomic::{AtomicUsize, Ordering};
-use crossbeam_epoch::{Guard, Owned};
+use crossbeam_epoch::{Atomic, Guard, Owned};
 use cs431::lockfree::list::{Cursor, List, Node};
+use std::mem::size_of;
+use std::ops::Deref;
+use std::thread::current;
+use std::usize;
 
 use super::growable_array::GrowableArray;
 use crate::NonblockingMap;
@@ -46,7 +50,54 @@ impl<V> SplitOrderedList<V> {
     /// Creates a cursor and moves it to the bucket for the given index.  If the bucket doesn't
     /// exist, recursively initializes the buckets.
     fn lookup_bucket<'s>(&'s self, index: usize, guard: &'s Guard) -> Cursor<'s, usize, Option<V>> {
-        todo!()
+        // todo!()
+        let bucket = self.buckets.get(index, guard);
+        let sent_key = index.reverse_bits();
+        let mut new_node: Owned<Node<usize, Option<V>>>;
+        loop {
+            let ori_sen_node = bucket.load(Ordering::SeqCst, guard);
+            if !ori_sen_node.is_null() {
+                return Cursor::new(bucket, ori_sen_node);
+            }
+            if index == 0 {
+                let mut head = self.list.head(guard);
+                new_node = Owned::new(Node::new(sent_key, None));
+                match head.insert(new_node, guard) {
+                    Ok(()) => {
+                        bucket.store(head.curr(), Ordering::SeqCst);
+                        return head;
+                    }
+                    Err(n) => {
+                        continue;
+                    }
+                }
+            }
+            let mut parent = self.size.load(Ordering::SeqCst);
+            loop {
+                parent >>= 1;
+                if parent <= index {
+                    break;
+                }
+            }
+            let parent_index = index - parent;
+            let mut prev_bucket = self.lookup_bucket(parent_index, guard);
+            let Ok(found) = prev_bucket.find_harris_michael(&sent_key, guard) else {
+                continue;
+            };
+            if found {
+                return prev_bucket;
+            }
+            let mut new_node = Owned::new(Node::new(sent_key, None));
+            match prev_bucket.insert(new_node, guard) {
+                Ok(()) => {
+                    bucket.store(prev_bucket.curr(), Ordering::SeqCst);
+                    return prev_bucket;
+                }
+                Err(n) => {
+                    continue;
+                }
+            }
+        }
     }
 
     /// Moves the bucket cursor returned from `lookup_bucket` to the position of the given key.
@@ -56,27 +107,86 @@ impl<V> SplitOrderedList<V> {
         key: &usize,
         guard: &'s Guard,
     ) -> (usize, bool, Cursor<'s, usize, Option<V>>) {
-        todo!()
+        // todo!()
+        let bucket_index = (*key) % self.size.load(Ordering::SeqCst);
+        let spl_ord_key = (key.reverse_bits()) | 1;
+        loop {
+            let mut cursor = self.lookup_bucket(bucket_index, guard);
+            if let Ok(found) = cursor.find_harris_michael(&spl_ord_key, guard) {
+                return (self.size.load(Ordering::SeqCst), found, cursor);
+            }
+        }
     }
 
     fn assert_valid_key(key: usize) {
         assert!(key.leading_zeros() != 0);
+    }
+
+    fn resize(&self, guard: &Guard) {
+        let ori_size = self.size.load(Ordering::SeqCst);
+        if self.count.load(Ordering::SeqCst) / ori_size > Self::LOAD_FACTOR {
+            let _ = self.size.compare_exchange(
+                ori_size,
+                ori_size * 2,
+                Ordering::SeqCst,
+                Ordering::SeqCst,
+            );
+        }
     }
 }
 
 impl<V> NonblockingMap<usize, V> for SplitOrderedList<V> {
     fn lookup<'a>(&'a self, key: &usize, guard: &'a Guard) -> Option<&'a V> {
         Self::assert_valid_key(*key);
-        todo!()
+        // todo!()
+        let (_, found, cursor) = self.find(key, guard);
+        if found {
+            let node = cursor.lookup().unwrap();
+            Some(node.as_ref().unwrap())
+        } else {
+            None
+        }
     }
 
     fn insert(&self, key: &usize, value: V, guard: &Guard) -> Result<(), V> {
         Self::assert_valid_key(*key);
-        todo!()
+        // todo!()
+        let spl_ord_key = key.reverse_bits() | 1;
+        let mut new_node = Owned::new(Node::new(spl_ord_key, Some(value)));
+        loop {
+            let (_, found, mut cursor) = self.find(key, guard);
+            if found {
+                return Err(new_node.into_box().into_value().unwrap());
+            }
+            match cursor.insert(new_node, guard) {
+                Err(n) => {
+                    new_node = n;
+                    continue;
+                }
+                Ok(()) => {
+                    self.count.fetch_add(1, Ordering::SeqCst);
+                    self.resize(guard);
+                    return Ok(());
+                }
+            }
+        }
     }
 
     fn delete<'a>(&'a self, key: &usize, guard: &'a Guard) -> Result<&'a V, ()> {
         Self::assert_valid_key(*key);
-        todo!()
+        // todo!()
+        loop {
+            let (_, found, cursor) = self.find(key, guard);
+            if !found {
+                return Err(());
+            }
+            match cursor.delete(guard) {
+                Err(()) => continue,
+                Ok(v) => {
+                    self.count.fetch_sub(1, Ordering::SeqCst);
+                    return Ok(v.as_ref().unwrap());
+                }
+            }
+        }
     }
 }
